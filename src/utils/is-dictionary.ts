6844b305bc0f6ee72f239a1956ba5ed2a18ea6d3
MrCheater <mister.cheater@list.ru>
2026-01-17 14:45:48 +0300

    chore: clean dist

--- src/utils/is-dictionary.ts
+++ src/utils/is-dictionary.ts
@@ -2,7 +2,7 @@ const simpleTypes = ["string", "number", "boolean"];
 
 export function isDictionary(data: any) {
   return (
-    Object(data) === data ||
+    Object(data) === data &&
     Object.values(data).every(
       (value) => value === null || simpleTypes.includes(typeof value),
     )
